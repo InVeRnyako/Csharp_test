12bf005f3658a55cf594884e0ab6285f5dfb3b69
InVeRnyako <inver.nyako@gmail.com>
2024-02-19 18:44:48 +0300

    shell works
    Предварительная работа по подготовке оболочки

--- area/Program.cs
+++ area/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа-оболочка для проверки работоспособности библиотеки
+
+
+using System.Collections;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder inputString = new StringBuilder(); 
+        if (inputString.Length > 0){ // Возможность програмного редактирования входящих данных по необходимости
+            // 
+        } else { 
+            // ! Проверки на правильность введенных данных на этом этапе не проводятся
+            Console.WriteLine("Выберите тип фигуры:\n1. Круг\n3. Треугольник");
+            inputString.Append(System.Console.ReadLine());
+            Console.WriteLine("Введите количество сторон фигуры (1 для круга)");
+            int ShapeSides = int.Parse(System.Console.ReadLine());
+            inputString.Append(';');
+            inputString.Append(ShapeSides.ToString());
+            for (int i = 0; i < ShapeSides; i++)
+            {
+                Console.WriteLine("Введите сторону #" + (i+1).ToString() + ":");
+                inputString.Append(';');
+                inputString.Append(System.Console.ReadLine());
+            }
+        }
+        Console.WriteLine(inputString.ToString());
+    }
+}
